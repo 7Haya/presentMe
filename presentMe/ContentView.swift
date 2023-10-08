5fb293dfa460657ecb69e0f753561ebafa7bbc8c
Haya Fahad <hayafahad46@gmail.com>
2023-10-08 17:03:23 +0300

    second

--- presentMe/ContentView.swift
+++ presentMe/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("Picture1").resizable()
+                .frame(width:100,height:100)
+                .cornerRadius(100)
+//                    .border(Color.black, width: 5)
+                .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.purple, lineWidth: 9)
+                        )
+            Text("Haya Fahad Alsabr")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(.gray)
+            Divider()
+            Text("I am a 20 years old, Programming student .Who enjoy drawing and watching Anime, Im a quick learner, I love to be an accomplished person.")
+                .foregroundColor(.blue)
+                .padding(.horizontal, 7.0)
         }
         .padding()
     }
