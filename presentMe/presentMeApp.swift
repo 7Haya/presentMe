435cf6079c67c39ee4f3e4f78330c51633373543
Haya Fahad <hayafahad46@gmail.com>
2023-10-08 16:41:44 +0300

    Initial Commit

---
+++ presentMe/presentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  presentMeApp.swift
+//  presentMe
+//
+//  Created by Haya Fahad Alsabr on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct presentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
